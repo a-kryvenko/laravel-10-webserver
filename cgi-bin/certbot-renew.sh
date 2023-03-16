ee8d3f5e501aac8b03f950b6fd81f609afdfa518
Andriy Kryvenko <krivenko.a.b@gmail.com>
2023-03-16 11:46:56 +0200

    Configuret whole laravel server

--- cgi-bin/certbot-renew.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BASEDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/../" &> /dev/null && pwd)
-
-cd "$BASEDIR/"
-
-docker-compose run --rm certbot renew && docker-compose kill -s SIGHUP webserver
-docker system prune -af
